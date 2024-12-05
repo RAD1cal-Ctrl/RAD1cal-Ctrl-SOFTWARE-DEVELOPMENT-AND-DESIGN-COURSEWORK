cfbe166957233d96dfd890f1666fcfdde4e4cd43
Raad <119070808+RAD1cal-Ctrl@users.noreply.github.com>
2024-12-05 23:52:01 +0000

    validation for the name and password attempt 1

---
+++ script.js
@@ -0,0 +1,20 @@
+const nameSection = document.getElementById('name')
+const passwordSection = document.getElementById ('password')
+const errorElement = document.getElementById('error')
+const formSection = document.getElementById ('form')
+
+Form.addEventListener('submit', (e)=>{
+    let messages = []
+    if (nameSection.value === ''|| nameSection.value == null) {
+        messages.push('Name is required')
+    }
+
+    if (passwordSection.value.length <= 6){
+        messages.push('Password must be longer then 6 characters')
+    }
+    if (messages.length > 0){
+        e.preventDefault()//prevents submition
+        errorElement.innerText = messages.join(', ') //joins messaged with ,
+    }
+})
+
