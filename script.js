9b1fad8ed5e856d25993c73974613468a4a44157
Raad <119070808+RAD1cal-Ctrl@users.noreply.github.com>
2024-12-12 23:07:54 +0000

    basic integration
    previous .js wasnt working so started with the basics

--- script.js
+++ script.js
@@ -1,27 +1,72 @@
-/* const nameSection = document.getElementById('name')
-const passwordSection = document.getElementById ('password')
-const errorElement = document.getElementById('error')
-const formSection = document.getElementById ('form')
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-Form.addEventListener('submit', (e)=>{
-    let messages = []
-    if (nameSection.value === ''|| nameSection.value == null) {
-        messages.push('Name is required')
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
     }
 
-    if (passwordSection.value.length <= 6){
-        messages.push('Password must be longer then 6 characters')
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
     }
-    if (messages.length > 0){
-        e.preventDefault()//prevents submition
-        errorElement.innerText = messages.join(', ') //joins messaged with ,
-    }
-}) */
 
-    function validatePassword() {
-        const passwordInput = document.getElementById('password');
-    
-        if (passwordInput.value.length < 6) {
-          alert("Password must be at least 6 characters long.");
-        }
-      }
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
